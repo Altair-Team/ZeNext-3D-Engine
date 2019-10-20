9ebc06b4dfe60a76b86abb2e0374f135f1b580af
Alivebyte <alivebyte.yt@gmail.com>
2019-10-20 18:34:11 +0300

    Обновил код и тест средств визуала для гита

--- ZeNext Engine/engine/engine.h
+++ ZeNext Engine/engine/engine.h
@@ -1,9 +1,13 @@
-
-
-
-
+//============================================================
+// ZeNextEngine Engine Core Code v0.0.1a
+// Made by Alivebyte!(alivebyte.yt@gmail.com)
+// Copyright Altair Team(C)2019-2019
+//============================================================
 
 class IEngine 
 {
 	public:
+	
+	virtual void Start();
+	virtual void Stop();
 }
